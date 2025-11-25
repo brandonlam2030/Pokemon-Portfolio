b8848ec7d8e993d08a3df14cee84c0a76e9b8a51
Brandon Lam <brandonlam2030@gmail.com>
2025-11-24 17:21:34 -0800

    Project page skills added

--- intro.js
+++ intro.js
@@ -1,19 +1,12 @@
 let screens = [".screenone",".screentwo",".screenthree",[".abt__me"], 
               [".project1",".project2",".project3",".project4",".project5",".project6"]];
 
-let screenone = true;
-let screentwo = true;
-let screenthree = true;
-let abt__me = false;
 
-
-
-
-
+let tracker = [];  
 const clickSound = new Audio("click.mp3");
 clickSound.volume = .3;
 document.addEventListener("keydown", (event) => {
-    if (event.key.toLowerCase() === 'z' || event.key.toLowerCase() === 'x') {
+    if (event.key == "ArrowRight" || event.key == "ArrowLeft" || event.key == "z" || event.key == "x") {
         clickSound.currentTime = 0;
         clickSound.play();
     }
@@ -40,20 +33,19 @@ function blink(elementID, interval) {
 blink('start', 700);
 blink('triangle__selector',400);
 
-function switchScene(classOne, classTwo, bool) {
+let curr = null;
+function forward(classOne, classTwo, show, isForward = true) {
     const sOne = document.querySelector(classOne);
     const sTwo = document.querySelector(classTwo);
-    let text_show = bool;
-
-    prev = classOne;
+    tracker.push(classOne);
+    console.log(tracker);
     curr = classTwo;
-    kdf__dialogue = sTwo.classList.contains("dialogue");
-    kdb__dialogue = sOne.classList.contains("dialogue");
+
 
     sOne.classList.add("show")
     const source = document.querySelector(`${classTwo} .typevalue`)
 
-    if (text_show) {
+    if (show) {
         setTimeout(() => {
         sOne.style.display='none';
         sTwo.style.display='flex';
@@ -76,19 +68,17 @@ function switchScene(classOne, classTwo, bool) {
 
 }
 
+function back() {
+    let sTwo = tracker.pop();
+    sOne.classList.add("show")
+    const source = document.querySelector(`${classTwo} .typevalue`)
 
+}
+let navbar__pos = 0;
 let step = 0;
 let cont = false;
-let kdf = false;
-let kdb = false;
 let screencount;
 let position = 1;
-let prev__dialogue;
-let prev = null;
-let curr = null;
-let kdf__dialogue = false;
-let kdb__dialogue = true;
-let target;
 let select = false;
 const boxes = document.querySelectorAll(".pbox");
 let index = 0;
@@ -144,76 +134,96 @@ document.addEventListener('keydown', (x) => {
 document.addEventListener('keydown', (event) => {
     const key = event.key.toLowerCase();
 
-    if (event.key === "ArrowLeft" && position!=0) {
-        position--;
-    } else if (event.key === "ArrowRight" && position!=2) {
-        position++;
-    }
+
+    if (step === 2) {
+        if (event.key === "ArrowLeft" && position!=0) {
+            position--;
+        } else if (event.key === "ArrowRight" && position!=2) {
+            position++;
+        }
 
 
-    if (position === 0) {
-        document.getElementById("triangle__selector").style.left = "25%";
-        document.getElementById("triangle__selector").style.top = "46%";
-    } else if (position === 1) {
-        document.getElementById("triangle__selector").style.left = "42%";
-        document.getElementById("triangle__selector").style.top = "54%";
-    } else if (position === 2) {
-        document.getElementById("triangle__selector").style.left = "64.5%";
-        document.getElementById("triangle__selector").style.top = "45%";
+        if (position === 0) {
+            document.getElementById("triangle__selector").style.left = "25%";
+            document.getElementById("triangle__selector").style.top = "46%";
+        } else if (position === 1) {
+            document.getElementById("triangle__selector").style.left = "42%";
+            document.getElementById("triangle__selector").style.top = "54%";
+        } else if (position === 2) {
+            document.getElementById("triangle__selector").style.left = "64.5%";
+            document.getElementById("triangle__selector").style.top = "45%";
+        }
     }
+    
 
-    
-    
-    
-    
-    if (curr) {
-        kdf__dialogue = document.querySelector(curr).classList.contains("dialogue");
-    } 
-    if (prev) {
-        kdb__dialogue = document.querySelector(prev).classList.contains("dialogue");
+    if (step === 4 && (event.key === "ArrowRight" || event.key === "ArrowLeft")) {
+        if (navbar__pos === 0 && event.key==="ArrowRight") {
+            navbar__pos++;
+            document.querySelector(".stat__box").style.visibility = "hidden";
+            document.querySelector(".navbar__main").style.borderBottom = "2px solid black";
+            document.querySelector(".skill__box").style.visibility = "visible";
+            document.querySelector(".navbar__skills").style.borderBottom = "none";
+
+
+        } else if (navbar__pos === 1 && event.key==="ArrowLeft") {
+            navbar__pos--;
+            document.querySelector(".stat__box").style.visibility = "visible";
+            document.querySelector(".navbar__main").style.borderBottom = "none";
+            document.querySelector(".skill__box").style.visibility = "hidden";
+            document.querySelector(".navbar__skills").style.borderBottom = "2px solid black";
+        }
     }
+    
+    
+    
+    
     
     
 
    if(key === 'z') {
         if (step === 0) {
-            switchScene(screens[step], screens[step+1], true);
+            forward(screens[step], screens[step+1], true);
             step++; 
             cont = false;
             screencount=0;
             return;
 
         } else if (step === 1 && cont) {
-            switchScene(screens[step],screens[step+1], true);
+            forward(screens[step],screens[step+1], true);
             step++;
             cont = false;
             screencount = 1;
             return;
         } else if (position === 0) {
-            switchScene(screens[step], screens[step+1][0], true);
+            forward(screens[step], screens[step+1][0], true);
             abt__hover = true;
             screencount = 2;
             position--;
             step++;
             return;
         } else if (select) {
-            switchScene(screens[step][0], screens[step+1][index], true);
+            forward(screens[step][0], screens[step+1][index], true);
             screencount = 3;
             select = false;
             step++;
+            navbar__page();
             return;
         }
    }
     
    if (key === 'x') {
-    
-    switchScene(screens[step], screens[step-1], true);
-    step--;
-    return;
+        back(screens[step])
    }
 
 
 })
+
+
+function navbar__page() {
+    
+    
+} 
+
 
 
 
